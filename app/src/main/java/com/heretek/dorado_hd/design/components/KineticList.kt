ef2593215f410b421b4a9f6ddc02fa7e9aab158f
John Smith <you@example.com>
2026-09-10 19:49:09 -0400

    chore(design): drop unused gesture imports in KineticList

--- app/src/main/java/com/heretek/dorado_hd/design/components/KineticList.kt
+++ app/src/main/java/com/heretek/dorado_hd/design/components/KineticList.kt
@@ -7,11 +7,8 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
